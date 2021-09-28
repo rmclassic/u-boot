a72fd8719048ea6f4c6c9118c6dbcd633765698d
Tobias Zagorni <tobias@zagorni.eu>
2021-09-28 22:56:32 +0300

    disk: part_efi: add partition detection workaround for eMMC
    Tegra devices on Android have proprietary partition table based on GPT.
    
    Signed-off-by: Tobias Zagorni <tobias@zagorni.eu>
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- disk/part_efi.c
+++ disk/part_efi.c
@@ -314,6 +314,9 @@ static int part_test_efi(struct blk_desc *dev_desc)
 {
 	ALLOC_CACHE_ALIGN_BUFFER_PAD(legacy_mbr, legacymbr, 1, dev_desc->blksz);
 
+	if (dev_desc->if_type == IF_TYPE_MMC && dev_desc->devnum == 0)
+		return 0;
+
 	/* Read legacy MBR from block 0 and validate it */
 	if ((blk_dread(dev_desc, 0, 1, (ulong *)legacymbr) != 1)
 		|| (is_pmbr_valid(legacymbr) != 1)) {

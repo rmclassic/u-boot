7ba5bbfa67fc97a67edbaaf239e4e679d9493e7e
Svyatoslav Ryhel <clamor95@gmail.com>
2022-01-31 22:29:29 +0200

    tf358768: add hw configuration
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/video/bridge/tc358768.c
+++ drivers/video/bridge/tc358768.c
@@ -37,8 +37,55 @@ static int tc358768_set_backlight(struct udevice *dev, int percent)
 	return -ENOSYS;
 }
 
+static void tc358768_hw_enable(struct udevice *dev)
+{
+	struct tc358768_priv *uc_priv = dev_get_uclass_priv(dev);
+	int ret;
+
+//	ret = clk_prepare_enable(uc_priv->refclk);
+//	if (ret)
+//		debug("%s: error enabling refclk (%d)\n", __func__, ret);
+
+	ret = regulator_set_enable_if_allowed(uc_priv->vddc, true);
+	if (ret)
+		debug("%s: error enabling vddc (%d)\n", __func__, ret);
+
+	ret = regulator_set_enable_if_allowed(uc_priv->vddmipi, true);
+	if (ret)
+		debug("%s: error enabling vddmipi (%d)\n", __func__, ret);
+
+	msleep(10);
+
+	ret = regulator_set_enable_if_allowed(uc_priv->vddio, true);
+	if (ret)
+		debug("%s: error enabling vddio (%d)\n", __func__, ret);
+
+	usleep_range(200, 300);
+
+	/*
+	 * The RESX is active low (GPIO_ACTIVE_LOW).
+	 * DEASSERT (value = 0) the reset_gpio to enable the chip
+	 */
+	ret = dm_gpio_set_value(&uc_priv->reset, false);
+	if (ret)
+		debug("%s: error changing reset-gpio (%d)\n", __func__, ret);
+
+	/* wait for encoder clocks to stabilize */
+	usleep_range(1000, 2000);
+}
+
 static int tc358768_attach(struct udevice *dev)
 {
+//	struct dm_i2c_chip *chip = dev_get_parent_plat(dev);
+//	struct i2c_msg msg[2];
+//	unsigned char buf[4] = { 0, 0, 0, 0 };
+//	int ret;
+
+	debug("%s: %s\n", __func__, dev->name);
+
+	/* Enable sequence */
+	tc358768_hw_enable(dev);
+
 	return 0;
 }
 

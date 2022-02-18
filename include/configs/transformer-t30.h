3a776139f20470a9d007613f49d7a386508de9ca
Svyatoslav Ryhel <clamor95@gmail.com>
2022-02-18 12:55:14 +0200

    asus: transformer-t30: use native u-boot setup
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- include/configs/transformer-t30.h
+++ include/configs/transformer-t30.h
@@ -13,15 +13,15 @@
 #include <linux/sizes.h>
 
 #include "tegra30-common.h"
-#include "transformer-common.h"
+
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"ASUS Transformer"
 
 /* VDD core PMIC */
 #define CONFIG_TEGRA_VDD_CORE_TPS62361B_SET3
 
 #ifdef CONFIG_TRANSFORMER_SPI_BOOT
 #define TRANSFORMER_VOLDOWN_ACTION \
-	"setenv gpio_vol_down 132;" \
-	"if run check_button;" \
 	"then echo Starting SPI flash update ...;" \
 		"run update_spi;"
 /* SPI */
@@ -29,29 +29,67 @@
 #define CONFIG_SPI_FLASH_SIZE          (4 << 20)
 #else
 #define TRANSFORMER_VOLDOWN_ACTION \
-	"setenv gpio_vol_down 131;" \
-	"if run check_button;" \
-	"then bootmenu;"
+	"then echo Starting U-Boot update ...;" \
+		"run update_ebt;"
 #endif
 
+#define TRANSFORMER_FLASH_SPI \
+	"update_spi=sf probe 0:1;" \
+		"echo Dumping current SPI flash content ...;" \
+		"sf read ${kernel_addr_r} 0x0 0x400000;" \
+		"if fatwrite mmc 1:1 ${kernel_addr_r} spi-flash-backup.bin 0x400000;" \
+		"then echo SPI flash content was successfully written into spi-flash-backup.bin;" \
+			"echo Reading bootloader binary;" \
+			"if load mmc 1:1 ${kernel_addr_r} bootloader-update.bin;" \
+			"then echo Writing bootloader into SPI flash;" \
+  				"sf probe 0:1;" \
+  				"sf update ${kernel_addr_r} 0x0 0x400000;" \
+				"poweroff;" \
+			"else echo Reading bootloader failed;" \
+				"poweroff; fi;" \
+		"else echo SPI flash backup FAILED! Aborting ...;" \
+			"poweroff; fi\0"
+
+#define TRANSFORMER_FLASH_EBT \
+	"update_ebt=echo Reading U-Boot binary;" \
+		"if load mmc 1:1 ${kernel_addr_r} ${bootloader_file};" \
+		"then echo Writing U-Boot into EBT;" \
+			"mmc dev 0 1;" \
+			"mmc write ${kernel_addr_r} 0x0 0x1000;" \
+		"else echo Reading U-Boot failed; fi\0"
+
 #define BOARD_EXTRA_ENV_SETTINGS \
-	TRANSFORMER_T30_EMMC_LAYOUT \
-	TRANSFORMER_DEFAULT_FILESET \
-	TRANSFORMER_BOOTZ \
-	TRANSFORMER_BUTTON_CHECK \
-	TRANSFORMER_BOOTMENU
+	"kernel_file=vmlinuz\0" \
+	"ramdisk_file=uInitrd\0" \
+	"bootloader_file=u-boot-dtb-tegra.bin\0" \
+	"bootkernel=bootz ${kernel_addr_r} - ${fdt_addr_r}\0" \
+	"bootrdkernel=bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}\0" \
+	TRANSFORMER_FLASH_SPI \
+	TRANSFORMER_FLASH_EBT
+
+#define TRANSFORMER_LOAD_KERNEL \
+	"echo Loading Kernel;" \
+	"if load mmc ${bootdev}:1 ${kernel_addr_r} ${kernel_file};" \
+	"then echo Loading DTB;" \
+		"load mmc ${bootdev}:1 ${fdt_addr_r} ${fdtfile};" \
+		"setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk${bootdev}p2 rw gpt;" \
+		"echo Loading Initramfs;" \
+		"if load mmc ${bootdev}:1 ${ramdisk_addr_r} ${ramdisk_file};" \
+		"then echo Booting Kernel;" \
+			"run bootrdkernel;" \
+		"else echo Booting Kernel;" \
+			"run bootkernel; fi;"
 
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
+	"if button VolumeDown;" \
 	TRANSFORMER_VOLDOWN_ACTION \
 	"else echo Loading from uSD...;" \
 		"setenv bootdev 1;" \
-		"setenv rootpart 2;" \
 		TRANSFORMER_LOAD_KERNEL \
 		"else echo Loading from uSD failed!;" \
 			"echo Loading from eMMC...;" \
 			"setenv bootdev 0;" \
-			"setenv rootpart 8;" \
 			TRANSFORMER_LOAD_KERNEL \
 			"else echo Loading Kernel FAILED! Turning power off;" \
 				"poweroff; fi;" \

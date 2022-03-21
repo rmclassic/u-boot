854d37f431b1caf9e21268df41cd516cce12583f
Ramin <raminterex@yahoo.com>
2022-03-22 01:32:44 +0430

    tegra30: headers clean up

--- arch/arm/mach-tegra/tegra30/nvboot_bct_t30.h
+++ arch/arm/mach-tegra/tegra30/nvboot_bct_t30.h
@@ -15,27 +15,27 @@
  * Defines the storage for a hash value (128 bits).
  */
 typedef struct nvboot_hash_rec {
-	uint32_t hash[NVBOOT_CMAC_AES_HASH_LENGTH];
+	u32 hash[NVBOOT_CMAC_AES_HASH_LENGTH];
 } nvboot_hash;
 
 
 typedef struct nv_bootloader_info_rec {
-	uint32_t version;
-	uint32_t start_blk;
-	uint32_t start_page;
-	uint32_t length;
-	uint32_t load_addr;
-	uint32_t entry_point;
-	uint32_t attribute;
+	u32 version;
+	u32 start_blk;
+	u32 start_page;
+	u32 length;
+	u32 load_addr;
+	u32 entry_point;
+	u32 attribute;
 	nvboot_hash crypto_hash;
 } nv_bootloader_info;
 
 typedef struct nvboot_config_table_rec {
-	uint32_t reserved0[0x8];
-	uint32_t boot_data_version;
-	uint8_t reserved1[0xf26];
-	uint32_t bootloader_used;
+	u32 reserved0[0x8];
+	u32 boot_data_version;
+	u8 reserved1[0xf26];
+	u32 bootloader_used;
 	nv_bootloader_info bootloader[NVBOOT_MAX_BOOTLOADERS];
-	uint32_t reserved2[0x1fc];
+	u32 reserved2[0x1fc];
 } nvboot_config_table;
 #endif /* #ifndef INCLUDED_NVBOOT_BCT_T30_H */
